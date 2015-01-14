056f5ac84b62c9bac9b4054fe9c837ab7d2e42c9
vaughn e. Itemuagbor <cab8021@gmail.com>
2015-01-14 22:36:06 +0100

    ui modifications
    half finished - simplifiction of the UI following Fear of rejection

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,44 +1,22 @@
 angular.module('starter.controllers', [
   'starter.services',
   'ionic.contrib.ui.cards'
+  
   ])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $location) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.controller('AppCtrl', function($scope, $ionicPopover, $location, $rootScope) {
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+ $ionicPopover.fromTemplateUrl('templates/popover.html', {
+    scope: $scope,
+  }).then(function(popover) {
+    $scope.popover = popover;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+$rootScope.navTitle='<img class="title-image" ng-src="img/logo2x.png" />';
 
-   // Return home on success
-  $scope.returnTo = function(view) {
-    $location.path(view)
-  };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('SessionsCtrl', function($scope, Session) {
